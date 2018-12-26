c0ec08db882c19ae323d316c3e4a6b59c025e2be
SoriTru <vandoren.trevor@gmail.com>
2018-12-26 15:28:20 -0800

    edited package.json to include correct root

---
+++ tictactoe/precache-manifest.23f829587fd09efdd85b90945ee9d650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "d64210a9c65e4dfa28d1",
+    "url": "/tictactoe/static/js/main.d64210a9.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tictactoe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "d64210a9c65e4dfa28d1",
+    "url": "/tictactoe/static/css/main.419d9e81.chunk.css"
+  },
+  {
+    "revision": "ff4555de85a8c8de2cfc0e2c21b6da0f",
+    "url": "/tictactoe/index.html"
+  }
+];
--- tictactoe/service-worker.js
+++ tictactoe/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d245fde1daef162f48bf442656902cc7.js"
+  "/tictactoe/precache-manifest.23f829587fd09efdd85b90945ee9d650.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tictactoe/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
